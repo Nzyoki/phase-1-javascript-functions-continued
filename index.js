c33d654ab00537e14e4f402440541da2434c9d24
Scolastica Nzyoki <solanzioki18@gmail.com>
2024-12-03 22:06:35 +0300

    new

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function mondayWork(toDo="go to the office"){
+    return `This Monday, I will ${toDo}.`;
+}
+
+
+function saturdayFun(toDo="roller-skate"){
+    return `This Saturday, I want to ${toDo}!`;
+
+}
+function wrapAdjective(par="*"){
+    return function(say="special"){
+        return `You are ${par}${say}${par}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("*");
+console.log(encouragingPromptFunction("a hard worker"));
+const encouragingPromptFunction2 = wrapAdjective("||");
+console.log(encouragingPromptFunction2("a dedicated programmer"));
